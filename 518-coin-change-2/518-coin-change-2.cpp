522a5a76080aac4d0e0ed2d50d6bec45c3f3b044
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-01-06 13:32:13 +0530

    Time: 12 ms (71.41%), Space: 7.3 MB (61.32%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -3,9 +3,9 @@ public:
     int change(int amount, vector<int>& coins) {
         vector<int>dp(amount+1,0);
         dp[0]=1;
-        sort(coins.begin(),coins.end());
+        // sort(coins.begin(),coins.end());
         for(int i=0;i<coins.size();i++){
-            if(coins[i]>amount) break;
+            // if(coins[i]>amount) break;
             for(int j=coins[i];j<=amount;j++){
                 dp[j]+=dp[j-coins[i]];
             }
