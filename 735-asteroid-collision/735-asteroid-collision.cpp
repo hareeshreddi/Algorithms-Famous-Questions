5a2ccebef7e8e3cf4ca800f38cee257fb7899919
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-01-16 21:10:31 +0530

    Time: 17 ms (37.15%), Space: 17.6 MB (62.57%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -5,21 +5,14 @@ public:
         s.reserve(asteroids.size());
         s.push_back(asteroids[0]);
         for(int i=1;i<asteroids.size();i++){
-            if(!s.empty()){
-                if(asteroids[i]>0 || s.back()<0)
-                    s.push_back(asteroids[i]);
-                else{
-                    bool f=false;
-                    while(!s.empty()){
-                        if(s.back()<0){f=true;break;}
-                        else if(-asteroids[i]>s.back()){s.pop_back();f=true;}
-                        else if(-asteroids[i]<s.back()) {f=false;break;}
-                        else{s.pop_back();f=false;break;}
-                    }
-                    if(f) s.push_back(asteroids[i]);
-                }
-            } else
+            if(s.empty() || asteroids[i]>0 || s.back()<0)
                 s.push_back(asteroids[i]);
+            else{
+                //keep popping +ve ele's which are less than -asteroids[i]
+                while(!s.empty()&&s.back()>0&&-asteroids[i]>s.back()) s.pop_back();
+                if(s.empty()||s.back()<0) s.push_back(asteroids[i]);
+                else if(asteroids[i]+s.back()==0) s.pop_back();
+            }
         }
         return s;
     }
