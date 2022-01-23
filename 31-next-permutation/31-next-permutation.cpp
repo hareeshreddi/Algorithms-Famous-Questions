cbba28601e1b652cac6c3210f9bf9836a3ddf2fc
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-01-23 17:13:37 +0530

    Time: 4 ms (76.49%), Space: 12.2 MB (35.51%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    //example 1,5,8,4,7,6,6,5,5,4,4,4,3,2,2,2,1
     void nextPermutation(vector<int>& nums) {
         int k=nums.size()-2;
         if(k==-1) return;
         while(k>=0 && nums[k]>=nums[k+1]) --k;
-        if(k>=0){//other than special case of k=-1(total non increasing sequence)
+        if(k>=0){
             int p=nums.size()-1;
             while(nums[p]<=nums[k]) --p;
             swap(nums[k++],nums[p]);
-        } else k=0;
-        for(int j=nums.size()-1;k<=j;++k,--j) swap(nums[k],nums[j]);
+        } else k=0;//special case of k=-1(total non increasing sequence)
+        for(int j=nums.size()-1;k<j;++k,--j) swap(nums[k],nums[j]);
     }
 };
