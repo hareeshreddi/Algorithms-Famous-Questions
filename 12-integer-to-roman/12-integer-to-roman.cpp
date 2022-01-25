8164e429b0afebddbd75e88a4693ed193abea0fe
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-01-25 22:10:56 +0530

    Time: 21 ms (17.64%), Space: 8.9 MB (24.39%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int>val={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<string>v={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        string ans="";
+        for(int i=0;i<v.size();i++){
+            if(!num) return ans;
+            while(num>=val[i]){
+                ans+=v[i];
+                num-=val[i];
+            }
+        }
+        return ans;
+    }
+};
