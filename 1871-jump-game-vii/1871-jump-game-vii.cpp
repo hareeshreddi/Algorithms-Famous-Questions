01199a629158c39018aa01d2e4050412126909d5
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-01-26 00:05:25 +0530

    Time: 52 ms (85.40%), Space: 17.8 MB (67.81%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     bool canReach(string s, int minJump, int maxJump) {
-        vector<bool>vis(s.length(),false);
-        vis[0]=true;
+        if(s[s.length()-1]=='1') return false;
         queue<int>q;
         q.push(0);
-        vis[0]=true;
         int max1=0;//covered until (for special case all 0;1 and max length)
-        while(!q.empty() && !vis[s.length()-1]){
+        while(!q.empty()){
             int r=q.front();
             int m1=max(r+minJump,max1+1),m2=min(r+maxJump,(int)s.length()-1);
+            if(m1<=m2 && m2==s.length()-1) return true;
             q.pop();
-            for(int p=m1;p<=m2;++p){
-                if(s[p]=='0'&&!vis[p]){
-                    vis[p]=true;
-                    q.push(p);
-                }
-            }
-            max1=max(max1,r+maxJump);
+            for(int p=m1;p<=m2;++p)
+                if(s[p]=='0') q.push(p);
+            max1=r+maxJump;
         }
-        return vis[s.length()-1];
+        return false;
     }
 };
