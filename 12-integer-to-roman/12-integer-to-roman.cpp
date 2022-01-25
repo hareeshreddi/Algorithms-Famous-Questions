fd9ed1d5fe0fbec390a3a68e5a6acd38141b5c1e
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-01-25 22:14:55 +0530

    Time: 21 ms (17.64%), Space: 8.9 MB (24.39%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -4,8 +4,7 @@ public:
         vector<int>val={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         vector<string>v={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         string ans="";
-        for(int i=0;i<v.size();i++){
-            if(!num) return ans;
+        for(int i=0;i<v.size()&&num>0;i++){
             while(num>=val[i]){
                 ans+=v[i];
                 num-=val[i];
