9dd0e52e72f105c1b874578b3bd0cccbc295a5e5
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-01-26 00:16:39 +0530

    Time: 77 ms (39.75%), Space: 17.6 MB (67.91%) - LeetHub

--- 1871-jump-game-vii/1871-jump-game-vii.cpp
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -4,7 +4,7 @@ public:
         if(s[s.length()-1]=='1') return false;
         queue<int>q;
         q.push(0);
-        int max1=0;//covered until (for special case all 0;1 and max length)
+        int max1=0;//tells covered until (for special case all 0;1 and max length)
         while(!q.empty()){
             int r=q.front();
             int m1=max(r+minJump,max1+1),m2=min(r+maxJump,(int)s.length()-1);
@@ -12,8 +12,11 @@ public:
             q.pop();
             for(int p=m1;p<=m2;++p)
                 if(s[p]=='0') q.push(p);
-            max1=r+maxJump;
+            max1=m2;
         }
         return false;
     }
 };
+// "00111010"
+// 3
+// 5
