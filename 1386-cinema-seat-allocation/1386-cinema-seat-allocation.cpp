ce4fd20ac0e6b86f0601d09cd6c5e9184a1a96fc
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-02-02 20:29:13 +0530

    Time: 148 ms (97.06%), Space: 54.8 MB (84.22%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -4,12 +4,8 @@ public:
         unordered_map<int,int>um;
         //2,3,4,5 =>30; 4,5,6,7=>120; 6,7,8,9=>480
         for(const vector<int>&v:reservedSeats)
-            if(v[1]!=1 && v[1]!=10){
-                if(um.find(v[0])==um.end())
-                    um[v[0]]=1<<(v[1]-1);
-                else
-                    um[v[0]]|=(1<<(v[1]-1));
-            }
+            if(v[1]!=1 && v[1]!=10)
+                um[v[0]]|=(1<<(v[1]-1));
         int ans=0;
         for(const pair<int,int>&p:um){
             int k=0;
