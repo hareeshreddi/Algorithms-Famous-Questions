975ddf1aff5cc32ce7d50e091b9de7e2e128d9e9
Hareesh Reddi <hareeshreddi16161616@gmail.com>
2022-02-15 09:46:03 +0530

    Time: 28 ms (70.35%), Space: 13.3 MB (78.27%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,19 +8,24 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 private:
-    priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
+    struct func{
+        bool operator()( ListNode* &l1, ListNode* &l2){
+            return l1->val>l2->val;
+        }
+    };
+    priority_queue<ListNode*,vector<ListNode*>,func>pq;
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode* ans=NULL,*start=NULL;
-        if(lists.size()==0) return ans;
         for(auto l:lists){
             if(l!=NULL)
-                pq.push({l->val,l});
+                pq.push(l);
         }
         while(pq.size()){
-            ListNode* p=pq.top().second;pq.pop();
+            ListNode* p=pq.top();pq.pop();
             if(ans==NULL) {
                 ans=p;start=p;
             }
@@ -29,7 +34,7 @@ public:
                 ans=p;
             }
             if(p->next)
-                pq.push({p->next->val,p->next});
+                pq.push(p->next);
         }
         return start;
     }
